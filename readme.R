e24b1edd71123927cc0519dea6d15fb054fd2894
Bryce Robinette <72757024+BryceRobinette@users.noreply.github.com>
2020-10-23 23:30:00 -0600

    Add files via upload

---
+++ readme.R
@@ -0,0 +1,14 @@
+##############################################################################################
+# Folder Description
+#############################################################################################
+#
+# The "ElectoralVoteData folder contains the files that were pulled from the internet.
+#
+# These files and the data inside them need to be curated and fixed
+#
+# Each file contains the data from the year in the name of the file
+#
+#
+# The curated and combined data files can be found in the MyData folder in the same repository
+#
+#
