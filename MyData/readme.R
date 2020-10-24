99dd00b6ea412b07189c7cfb99473bc3373a8424
Bryce Robinette <72757024+BryceRobinette@users.noreply.github.com>
2020-10-23 23:30:59 -0600

    Add files via upload
    Curated Data Folder

---
+++ MyData/readme.R
@@ -0,0 +1,12 @@
+##############################################################################################
+# Folder Description
+#############################################################################################
+#
+# The "MyData folder contains the files that were curated by the electCollege.R file.
+#
+# Each file contains the data from the year in the name of the file
+#
+#
+# The file electData.csv is the combined total dataset we want to work with
+#
+#
