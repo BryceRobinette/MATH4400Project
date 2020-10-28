de33fc03aa00376512dcd1671650fb36b9312c21
kurstenreznik <72758183+kurstenreznik@users.noreply.github.com>
2020-10-28 13:17:58 -0600

    Update TwitterProj.v1.R

--- TwitterProj.v1.R
+++ TwitterProj.v1.R
@@ -84,4 +84,23 @@ ggplot(data = sentiment.biden, aes(x=sentiment, y= Score))+geom_bar(aes(fill=sen
   theme(legend.position = "none") +
   xlab("Sentiments")+ylab("scores") + ggtitle("Sentiments of Biden")
 
+#Creating Corpus----------------------------------------------------------------
+
+biden.corpus.clean = Corpus(VectorSource(biden.text))
+trump.corpus.clean = Corpus(VectorSource(trump.text))
+
+biden.trump.corpus.clean = Corpus(VectorSource(c(biden.text,trump.text)))
+
+y= c( rep(1, length(biden.corpus.clean)) , rep(0, length(trump.corpus.clean)))
+
+#WordCloud------------------------------------------------------------------------------------------
+
+library(wordcloud)
+library(RColorBrewer)
+
+wordcloud(biden.corpus.clean, min.freq = 10, colors = brewer.pal(8,"Dark2"),max.words = 50)
+wordcloud(trump.corpus.clean, min.freq = 10, colors = brewer.pal(8,"Dark2"),max.words = 50)
+
+
+
 
